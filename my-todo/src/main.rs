27cd730adaf5ab909ddd2b8cbfa06c34abd9ba5c
y0-suke <yo.kajittaringo@gmail.com>
2023-07-06 00:16:37 +0900

    入力したusernameに対して固定のidを紐づけて変えすPOST /users の処理を追加した

--- my-todo/src/main.rs
+++ my-todo/src/main.rs
@@ -1,4 +1,11 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json,
+    Router
+};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::env;
 
@@ -9,7 +16,9 @@ async fn main() {
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
 
@@ -21,4 +30,26 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, world!"
+}
+
+async fn create_user(
+    Json(payload): Json<CreateUser>
+) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
